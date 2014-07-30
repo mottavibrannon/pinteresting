f93d155a8f7ce614b0a4483a7f2abaf060234869
Michael Ottavi-Brannon <mottavibrannon@gmail.com>
2014-07-29 23:27:10 -0700

    Added user input fields for usernames and first/last names. NEED to update for actual value storage

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140719020355) do
+ActiveRecord::Schema.define(version: 20140730052117) do
 
   create_table "dishes", force: true do |t|
     t.string   "name"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20140719020355) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username"
+    t.string   "firstName"
+    t.string   "lastName"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
