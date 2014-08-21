d3dab66a5694b9579e33ab5bb34056f74ac150a8
Michael Ottavi-Brannon <mottavibrannon@gmail.com>
2014-08-20 22:12:18 -0700

    Added Amazon Cloudfront CDN

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,6 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  # SETTING TO TRUE TO ALLOW LOADING OF IMAGES ON HEROKU
   config.assets.compile = false
 
   # Generate digests for assets URLs.
@@ -56,7 +55,8 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = "http://assets.example.com"
+  # Setting CDN to Amazon Cloudfront 
+  config.action_controller.asset_host = "dh80khbjsw9oq.cloudfront.net"
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
